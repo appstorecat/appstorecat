857aad1145095c7b6825ccdbcb295161237336eb
ismailcaakir <ismailcakirn@gmail.com>
2026-04-17 18:04:07 +0300

    chore: slow sync schedules to daily for dev testing

--- server/routes/console.php
+++ server/routes/console.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('appstorecat:apps:sync-discovery --ios')->everyMinute();
-Schedule::command('appstorecat:apps:sync-discovery --android')->everyMinute();
-Schedule::command('appstorecat:apps:sync-tracked --ios')->everyMinute();
-Schedule::command('appstorecat:apps:sync-tracked --android')->everyMinute();
+Schedule::command('appstorecat:apps:sync-discovery --ios')->daily();
+Schedule::command('appstorecat:apps:sync-discovery --android')->daily();
+Schedule::command('appstorecat:apps:sync-tracked --ios')->daily();
+Schedule::command('appstorecat:apps:sync-tracked --android')->daily();
 
 Schedule::command('appstorecat:charts:sync-daily --ios')->dailyAt('00:30');
 Schedule::command('appstorecat:charts:sync-daily --android')->dailyAt('00:30');
